e32e58b2e573f7d8c8525f45d63c657d47ab4233
David Stewart <david.stewart@communities-ni.gov.uk>
2019-01-11 10:21:00 +0000

    Add project files.

---
+++ MoqDemo-CreditCardsApps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoqDemo_CreditCardsApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
