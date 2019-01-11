246fff16786945f698b0bb5cd7d3bc350808a41e
David Stewart <david.stewart@communities-ni.gov.uk>
2019-01-11 13:38:28 +0000

    first tests

---
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+
+namespace CreditCardApplications.Tests
+{
+    public class CreditCardApplicationEvaluatorShould
+    {
+        [Fact]
+        public void AcceptHighIncomeApplications()
+        {
+            var sut = new CreditCardApplicationEvaluator();
+            var application = new CreditCardApplication
+            {
+                GrossAnnualIncome = 100000
+            };
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.AutoAccepted, decision);
+        }
+
+
+
+        [Fact]
+        public void ReferYoungApplicants()
+        {
+            var sut = new CreditCardApplicationEvaluator();
+            var application = new CreditCardApplication
+            {
+                Age = 19
+            };
+
+            CreditCardApplicationDecision decision = sut.Evaluate(application);
+            Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decision);
+        }
+    }
+}
